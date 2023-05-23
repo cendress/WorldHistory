f2f4c64be34dadbbc51df30bd3ed8380d8ec815a
cendress <95492688+cendress@users.noreply.github.com>
2023-05-23 13:34:17 -0400

    Complete Prehistory reading section

--- World History/Controllers/HomeViewController.swift
+++ World History/Controllers/HomeViewController.swift
@@ -45,7 +45,7 @@ class HomeViewController: UIViewController {
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      if segue.identifier == "goToPrehistoricTimes" {
+      if segue.identifier == "goToPrehistory" {
       }
   }
 
---
+++ World History/Models/ReadingText.swift
@@ -0,0 +1,53 @@
+//
+//  Text.swift
+//  World History
+//
+//  Created by Christopher Endress on 5/23/23.
+//
+
+import Foundation
+
+struct ReadingText {
+  
+  let prehistoryText = """
+The story of our human ancestors begins in the hazy depths of prehistory, long before the invention of writing, in a time when our understanding of the past relies entirely on archaeological evidence. It's an era of early human history that spans from the initial evolution of Homo sapiens in Africa around 300,000 years ago, up to the emergence of the earliest civilizations around 3000 BCE. This remarkable journey traces humanity's first steps, the taming of fire, the development of tools, the transition from nomadic life to settled farming, and the eventual establishment of complex societies.
+
+The journey commences with the Paleolithic era, or Old Stone Age. In this epoch, which spans from around 2.6 million years ago until about 10,000 BCE, humans were entirely dependent on hunting and gathering for survival. They lived in small, mobile groups and their lives were inextricably bound to the rhythms of nature. The men typically hunted large game while women gathered fruits, nuts, and other plant-based food. Despite these harsh conditions, the ingenuity of our ancestors became evident in the Paleolithic era, as they started to create tools. The earliest tools were simple and made from stone, wood, and bone, but over time they became more complex and specialized.
+
+One of the most pivotal moments in our ancestors' existence was the mastery of fire. Fire not only provided warmth in the cold, protection from predators, and a method for cooking food, but it also played a vital role in socialization. Around a hearth, early humans could extend their day into the night, share stories, strengthen social bonds, and transmit knowledge.
+
+Moving forward, during the last ice age, humans began to demonstrate a sense of spirituality and art. The cave paintings of Lascaux in France and Altamira in Spain offer fascinating insights into their artistic skills and symbolic thinking. These paintings often depicted animals, hinting at a deep connection and reverence towards the natural world. Some archaeologists argue that these paintings might have been part of hunting rituals or held religious significance.
+
+The end of the Paleolithic era coincided with the end of the last Ice Age, leading to significant climate changes. This marked the dawn of the Mesolithic, or Middle Stone Age, a transitional period that varied greatly in length depending on the region. The warmer climate caused large-scale migration of animals and, subsequently, humans. This period saw the development of more refined stone tools and the first evidence of fishing tools, indicating a broader diet.
+
+This sets the stage for one of the most profound shifts in human history: the transition from the nomadic hunter-gatherer lifestyle of our ancestors to a settled, agriculture-based existence.
+
+The Neolithic Revolution, or the New Stone Age, saw the emergence of agriculture and settled life. This period, which began around 10,000 BCE and continued until the invention of writing, was marked by several technological advances and the growth of complex societies.
+
+In the Fertile Crescent, an area in the Middle East spanning modern-day Iraq, Syria, Lebanon, Israel, Palestine, Jordan, Egypt, and parts of Turkey and Iran, people began to experiment with a revolutionary new way of life. They started to domesticate plants and animals. Wild grains were selectively bred for desirable traits, leading to the first crops, including barley and wheat. Animals like goats, sheep, and cattle were also tamed, providing a reliable source of meat, milk, and hides. This marked a significant departure from the unpredictable and often dangerous life of hunting and gathering.
+
+The shift to agriculture allowed human groups to stay in one place, leading to the establishment of the world's first villages. Some of the earliest known settlements include Jericho in the West Bank and Çatalhöyük in modern-day Turkey. These were complex societies, with houses, communal spaces, and even rudimentary forms of government.
+
+The rise of agriculture led to an explosion in the human population. With more food available, the population grew rapidly, which in turn led to more labor for farming and construction, creating a cycle of growth. However, this shift also brought new challenges. Farming was hard work, and people were exposed to diseases from animals and crowded living conditions. The dependence on crops also made societies vulnerable to poor harvests and changing weather conditions.
+
+Despite these challenges, the benefits of farming were clear. The surplus food allowed some people to specialize in jobs other than food production for the first time. Craftsmen could devote their time to developing new types of tools and goods, while others could focus on governing, planning, and organizing communal life. The newfound stability also provided an environment that fostered the growth of culture and the arts.
+
+Toward the end of the Neolithic era, a new material began to reshape society: metal. Humans discovered that heating certain types of rocks produced a substance that could be molded when hot and would hold an edge when cool.
+
+The development of metallurgy, specifically the creation of bronze — an alloy of copper and tin, had far-reaching effects on human society. This marked the start of the first true "technological age," transforming tools, weaponry, and leading to significant societal changes.
+
+Early societies soon realized that bronze was harder and more durable than other materials they had access to, and so they used it to produce a range of tools and weapons. Over time, they developed sophisticated methods of casting and forging. This enabled mass production and the creation of more efficient farming tools, leading to even greater agricultural output.
+
+One of the most momentous advancements of the Bronze Age was the invention of the wheel. This revolutionary technology drastically improved transportation and trade, allowing societies to exchange goods and ideas over vast distances. The invention of the plow further boosted agriculture, allowing for the cultivation of larger fields and increasing food production.
+
+The surplus of resources brought about by these innovations led to increased population density, paving the way for the rise of cities. These early cities, such as Uruk and Ur in Mesopotamia, Harappa in the Indus Valley, and Memphis in Egypt, were the precursors to the great civilizations of antiquity. As these cities grew, so did the need for organization and administration, leading to the development of the earliest forms of government.
+
+It was also during this period that writing was invented, a momentous leap that marked the end of prehistory and the dawn of history. The earliest known writing system, cuneiform, emerged in Sumer in Mesopotamia around 3200 BCE. This system started as simple pictographs, gradually evolving into a complex system of symbols representing sounds and ideas.
+
+Cuneiform was used to record laws, economic transactions, and even literature, such as the Epic of Gilgamesh, one of the oldest known works of literature. Around the same time, ancient Egyptians developed their own writing system, hieroglyphics.
+
+Towards the end of the Bronze Age, around 1200 BCE, a mysterious and widespread collapse affected many of the great civilizations of the Mediterranean and the Near East. This paved the way for the rise of new civilizations and marked the transition into the Iron Age.
+
+As we conclude our journey through prehistory, we stand at the brink of recorded history, looking forward to the advent of the great civilizations of the ancient world, the focus of our next exploration. Thus, from the earliest tool-making hominids of the Paleolithic to the complex city-states of the Bronze Age, the story of prehistory is a testament to human ingenuity and the relentless drive towards progress.
+"""
+}
