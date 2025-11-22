ccbd2f0773378ecb6e00e9de635ed381e987b0f3
cendress <95492688+cendress@users.noreply.github.com>
2025-11-22 15:42:29 -0500

    Create Haptic enum utility

---
+++ World History/SwiftUI/Utils/Haptic.swift
@@ -0,0 +1,32 @@
+//
+//  Haptic.swift
+//  World History
+//
+//  Created by Christopher Endress on 11/22/25.
+//
+
+import SwiftUI
+
+enum Haptic {
+    static func light() {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+    }
+    static func medium() {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    }
+    static func heavy() {
+        UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+    }
+    static func success() {
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+    }
+    static func warning() {
+        UINotificationFeedbackGenerator().notificationOccurred(.warning)
+    }
+    static func error() {
+        UINotificationFeedbackGenerator().notificationOccurred(.error)
+    }
+    static func selection() {
+        UISelectionFeedbackGenerator().selectionChanged()
+    }
+}
